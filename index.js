8a6d422f1747fb450d4eb3d5fd39a00e98b0db63
simalexan <alexander.simovic@gmail.com>
2018-04-03 23:31:32 +0200

    updated from the stream on 03.04.2018

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 'use strict';
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v4');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = process.env.TABLE_NAME;
 
-exports.handler = (event, context, callback) => {
-    console.log(event);
+exports.putViewers = (event, context, callback) => {
+    let { nickname } = JSON.parse(event.body);
+
+    let params = {
+        TableName: TABLE_NAME,
+        Item: {
+            viewersId: uuid(),
+            twitchNickName: nickname
+        }
+    };
+
+    dynamo.put(params).promise()
+        .then(response => {
+            console.log(response);
+            reply(201, response, callback);
+        }).catch(err => {
+            console.log(err);
+            reply(400, err, callback);
+        });
+};
+
+exports.getViewers = (event, context, callback) => {
+
+    let params = {
+        TableName: TABLE_NAME
+    };
+
+    dynamo.scan(params).promise()
+        .then(response => {
+            console.log(response);
+            reply(200, response.Items, callback);
+        }).catch(err => {
+            console.log(err);
+            reply(400, err, callback);
+        });
+};
+
+function reply(statusCode, data, callback){
     callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(event),
+        statusCode: statusCode,
+        body: JSON.stringify(data),
         headers: { 'Content-Type': 'application/json' }
     });
-};
+}
